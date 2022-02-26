adca07866d1855dd9d8b7793677aa1b35f04785c
Katelyn Bunker <kerbunker@gmail.com>
2022-02-26 17:39:28 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budgeters-friend', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
